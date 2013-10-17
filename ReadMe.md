cbef3cc67ae733d713108de83b6bac94690fbb83
MikeCoder <mikecoder13@gmail.com>
2013-10-17 19:27:44 +0800

    算法-排序
    归并排序

--- ReadMe.md
+++ ReadMe.md
@@ -9,5 +9,5 @@
 >* [**中缀表达式转后缀表达式**](https://github.com/MikeCoder/MyJavaStudy/blob/master/%E7%BB%8F%E5%85%B8%E7%9A%84%E7%AE%97%E6%B3%95%E5%AE%9E%E7%8E%B0/%E4%B8%AD%E7%BC%80%E8%A1%A8%E8%BE%BE%E5%BC%8F%E8%BD%AC%E5%90%8E%E7%BC%80%E8%A1%A8%E8%BE%BE%E5%BC%8F.java)
 >* [**计算后缀表达式**](https://github.com/MikeCoder/MyJavaStudy/blob/master/%E7%BB%8F%E5%85%B8%E7%9A%84%E7%AE%97%E6%B3%95%E5%AE%9E%E7%8E%B0/%E8%AE%A1%E7%AE%97%E5%90%8E%E7%BC%80%E8%A1%A8%E8%BE%BE%E5%BC%8F.java)
 >* [**卡特兰数**](https://github.com/MikeCoder/MyJavaStudy/blob/master/%E7%BB%8F%E5%85%B8%E7%9A%84%E7%AE%97%E6%B3%95%E5%AE%9E%E7%8E%B0/%E5%8D%A1%E7%89%B9%E5%85%B0%E6%95%B0%28%E7%BB%84%E5%90%88%E6%95%B0%E5%AD%A6%29.md)
-
+>* [**各种排序算法**]()
 Wish You a Happy Day!
---
+++ 经典的算法实现/各种排序算法.java
@@ -0,0 +1,80 @@
+package iot.mike.othertest.somethingelse;
+
+import java.util.Random;
+
+public class 各种排序算法 {
+	public static void main(String[] args) {
+		Random random = new Random();
+		int[] testnums = new int[10];
+		for (int i = 0; i < testnums.length; i++) {
+			testnums[i] = random.nextInt(20);
+		}
+		for (int i : testnums) {
+			System.out.println(i);
+		}
+		mergesort(testnums, 0, testnums.length - 1);
+		for (int i : testnums) {
+			System.out.println(i);
+		}
+	}
+	
+	/**
+	 * 进行归并排序,从小到大。
+	 * @param nums 排序数组
+	 * @param i 起始地址
+	 * @param j 结束地址
+	 */
+	public static void mergesort(int[] nums, int i, int j) {
+		if (j - i == 0) {
+			return;
+		}
+		
+		if (j - i == 1) {
+			if (nums[i] >= nums[j]) {
+				int temp = nums[j];
+				nums[j] = nums[i];
+				nums[i] = temp;
+				return;
+			}else {
+				return;
+			}
+		}
+		
+		if (j - i >= 2) {
+			int m = (i + j)/2;
+			mergesort(nums, i, m);
+			mergesort(nums, m + 1, j);
+			
+			int[] nums_t = new int[j - i + 1];
+			int x = i, y = m + 1;
+			for (int k = 0; k < nums_t.length; k++) {
+				if (nums[x] > nums[y]) {
+					nums_t[k] = nums[y];
+					y++;
+					if (y == j + 1) {
+						for (int k2 = x; k2 <= m; k2++) {
+							k++;
+							nums_t[k] = nums[k2];
+						}
+						break;
+					}
+				}else {
+					nums_t[k] = nums[x];
+					x++;
+					if (x == m + 1) {
+						for (int k2 = y; k2 <= j; k2++) {
+							k++;
+							nums_t[k] = nums[k2];
+						}
+						break;
+					}
+				}
+			}
+			for (int k = 0; k < nums_t.length; k++) {
+				nums[i] = nums_t[k];
+				i++;
+			}
+			return;
+		}
+	}
+}
