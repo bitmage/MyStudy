1f4e59a0977347012441a6f381ee28e304fc9eb9
Mike Tang <mike@mikecoder.cn>
2017-03-29 14:10:30 +0800

    update english blog

--- MyBlogs/en/publish.sh
+++ MyBlogs/en/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-hexo g && hexo s
+hexo clean && hexo g && hexo s
 rm -rf ~/Workspace/fun-code/oj-code/docs
 cp -r public ~/Workspace/fun-code/oj-code/docs
 cd ~/Workspace/fun-code/oj-code/ && git add . && git commit -am 'update blog' && git push origin master
